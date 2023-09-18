a08226015600624c621b2554931d96296bdda953
Måns Edenfalk <Medenfalk@gmail.com>
2023-09-18 22:58:01 +0200

    Added restaurant type and created restaurant collection

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,8 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import {
+	CollectionReference,
+	DocumentData,
+	collection,
+	getFirestore,
+} from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+import { Restaurant } from '../types/Restaurant.types'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -28,10 +34,10 @@ export const db = getFirestore(app)
 // Get Storage instance
 export const storage = getStorage(app)
 
-//const createCollection = <T = DocumentData>(collectionName: string) => {
-//	return collection(db, collectionName) as CollectionReference<T>
-//}
+const createCollection = <T = DocumentData>(collectionName: string) => {
+	return collection(db, collectionName) as CollectionReference<T>
+}
 
-//export const dummyCol = createCollection<never>("stupid")
+export const restaurantCol = createCollection<Restaurant>('restaurant')
 
 export default app
---
+++ src/types/Restaurant.types.ts
@@ -0,0 +1,14 @@
+export type Restaurant = {
+	_id: string
+	address: string
+	name: string
+	website: string
+	instagram: string
+	facebook: string
+	category: string
+	offer: string
+	latLng: {
+		lat: number
+		lng: number
+	}
+}
