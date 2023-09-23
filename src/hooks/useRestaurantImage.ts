ad3cc0a38b900c771f9e5db66339b4a9aaecee17
Malin Kyttä <malinkytta@gmail.com>
2023-09-23 12:33:08 +0200

    Removed hooks because changed name (for images)

--- src/hooks/useRestaurantImage.ts
+++
@@ -1,9 +0,0 @@
-import { restaurantImageCol } from '../services/firebase'
-import { RestaurantImage } from '../types/Restaurant.types'
-import useStreamCollection from './useStreamCollection'
-
-const useRestaurantImage = () => {
-	return useStreamCollection<RestaurantImage>(restaurantImageCol)
-}
-
-export default useRestaurantImage
