2b22648cbe866a1fb5d6a42ccbb102c624b8412c
vpettersson <viktor.h.pettersson@gmail.com>
2023-09-28 14:23:28 +0200

    centered not found title

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,8 @@
 const NotFound = () => {
 	return (
-		<>
-			<h1>This page can not be found</h1>
-		</>
+		<div className='not-found'>
+			<h1 className='not-found-title'>This page can not be found</h1>
+		</div>
 	)
 }
 
